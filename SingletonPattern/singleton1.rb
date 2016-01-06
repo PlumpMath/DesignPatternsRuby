2b641347757993c5756b94024423228375718536
alejo <alejorodasvasquez@gmail.com>
2016-01-05 22:00:51 -0500

    Singleton Pattern implemented, you must to put @@instance = SimpleLogger.new after the others methods but before the class method self.instance

---
+++ SingletonPattern/singleton1.rb
@@ -0,0 +1,38 @@
+class SimpleLogger
+  attr_accessor :level
+
+  ERROR = 1
+  WARNING = 2
+  INFO = 3
+
+
+  def initialize
+    @log = File.open("log.txt", "w")
+    @level = WARNING
+  end
+
+  def error msg
+    @log.puts(msg)
+    @log.flush
+  end
+
+  def warning msg
+    @log.puts(msg) if @level >= WARNING
+    @log.flush
+  end
+
+  def info msg
+    @log.puts(msg) if @level >= INFO
+    @log.flush
+  end
+
+  @@instance = SimpleLogger.new
+
+  def self.instance
+    return @@instance
+  end
+end
+
+SimpleLogger.instance.info("Computer wins chess game")
+SimpleLogger.instance.warning('AE-35 hardware failure predicted.')
+SimpleLogger.instance.error('HAL-9000 malfunction, take emergency action!')
